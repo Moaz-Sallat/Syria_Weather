e7b270d257fee3e2656f8603a913bab99b7e6ad1
Imad Aladl <imad.aladl.10@gmail.com>
2026-05-13 16:07:08 +0300

    Add WeatherMap.vue component for toggling weather layers

---
+++ src/components/WeatherMap.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div id="map" style="height: 500px; width: 100%;"></div>
+    <button @click="toggleLayer('wind')">Toggle Wind Layer (تبديل طبقة الرياح)</button>
+    <button @click="toggleLayer('temperature')">Toggle Temperature Layer (تبديل طبقة الحرارة)</button>
+  </div>
+</template>
+
+<script>
+import weatherMap from './weatherMap';
+
+export default {
+  mixins: [weatherMap],
+};
+</script>
+
+<style>
+#map {
+  height: 600px;
+  width: 100%;
+}
+</style>
